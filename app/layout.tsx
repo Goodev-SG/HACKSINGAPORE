75fe5ce30fe268e267cb65993408dc2e2eb90015
Thant Htet Aung <maung.good@gmail.com>
2024-05-25 15:23:17 +0800

    Clerk Added with SignIn/Out

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,8 +4,10 @@ import {
   SignedIn,
   SignedOut,
   UserButton,
+  SignOutButton,
 } from "@clerk/nextjs";
 import "./globals.css";
+
 export default function RootLayout({
   children,
 }: {
@@ -15,13 +17,24 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body>
-          <SignedOut>
-            <SignInButton />
-          </SignedOut>
-          <SignedIn>
-            <UserButton />
-          </SignedIn>
-          {children}
+          <header className="py-6 bg-primary px-4">
+            <div className="flex justify-between">
+              <div>
+                <a href="/">
+                  <img src="" alt="logo" />
+                </a>
+              </div>
+              <div>
+                <SignedOut>
+                  <SignInButton forceRedirectUrl="/dashboard" />
+                </SignedOut>
+                <SignedIn>
+                  <UserButton />
+                </SignedIn>
+              </div>
+            </div>
+          </header>
+          <main>{children}</main>
         </body>
       </html>
     </ClerkProvider>
